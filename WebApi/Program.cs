60d3ce0729486125fe791a32e87cf8e828d17068
ajnur ademi <ajnur.ademi@hotmail.com>
2024-05-27 12:09:46 +0200

    Version 1.1 | webapi with mongodb

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/check", () => {
+/* Code zur Prüfung der DB ...*/
+return "Minimal API Version 1.1";
+});
 
 app.Run();
